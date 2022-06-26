9dfd48d14f8a10fe01cc2a74afa1fba9de6c42d2
Sam I <samanan.itthiphinyo@gmail.com>
2022-06-26 05:36:33 -0400

    browse container now shows list of films

--- src/components/loading/loading.component.jsx
+++ src/components/loading/loading.component.jsx
@@ -1,5 +1,15 @@
 import React from "react";
+import { LockBody, Picture, Spinner } from "./loading.styles";
 
 export default function Loading(children, ...restProps) {
+    return(
+    <Spinner {...restProps}>
+        <LockBody />
+        <Picture src={`../../../public/images/users/${src}.png`} />
+    </Spinner>
+    );
+};
+
+Loading.Other = function LoadingOther(children, ...restProps) {
     return <Loading {...restProps}>{children}</Loading>
-}
+};
--- src/containers/browse.container.jsx
+++ src/containers/browse.container.jsx
@@ -1,18 +1,95 @@
 import React, { useContext, useEffect, useState } from "react";
 import { SelectProfileContainer } from "./profiles.containers";
 import { FirebaseContext } from "../context/firebase.context";
+import { Header, Loading, Card } from "../components/component.export";
+import logo from '../logo.svg';
+import * as ROUTES from "../env/routes";
 
-export function BrowseContainer({slides}) {
+export function BrowseContainer({ slides }) {
+    const [category, setCategory] = useState('series');
     const [profile, setProfile] = useState({});
     const [loading, setLoading] = useState(true);
-    const {firebase} = useContext(FirebaseContext);
+    const [searchTerm, setSearchTerm] = useState('');
+    const [slideRows, setSlideRows] = useState([]);
+  
+    const { firebase } = useContext(FirebaseContext);
     const user = firebase.auth().currentUser || {};
-
+  
     useEffect(() => {
-        setTimeout(() => {
-            setLoading(false);
-        }, 500);
+      setTimeout(() => {
+        setLoading(false);
+      }, 3000);
     }, [profile.displayName]);
-
-    return profile.displayName ? (loading ? <Loading src={user.photoURL} /> : null) : <SelectProfileContainer user={user} setProfile={setProfile} />
-};
+  
+    useEffect(() => {
+      setSlideRows(slides[category]);
+    }, [slides, category]);
+  
+    return profile.displayName ? (
+      <>
+        {loading ? <Loading src={user.photoURL} /> : <Loading.ReleaseBody />}
+  
+        <Header src="joker1" dontShowOnSmallViewPort>
+          <Header.Frame>
+            <Header.Group>
+              <Header.Logo to={ROUTES.HOME} src={logo} alt="Netflix" />
+              <Header.TextLink active={category === 'series' ? 'true' : 'false'} onClick={() => setCategory('series')}>
+                Series
+              </Header.TextLink>
+              <Header.TextLink active={category === 'films' ? 'true' : 'false'} onClick={() => setCategory('films')}>
+                Films
+              </Header.TextLink>
+            </Header.Group>
+            <Header.Group>
+              <Header.Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+              <Header.Profile>
+                <Header.Picture src={user.photoURL} />
+                <Header.Dropdown>
+                  <Header.Group>
+                    <Header.Picture src={user.photoURL} />
+                    <Header.TextLink>{user.displayName}</Header.TextLink>
+                  </Header.Group>
+                  <Header.Group>
+                    <Header.TextLink onClick={() => firebase.auth().signOut()}>Sign out</Header.TextLink>
+                  </Header.Group>
+                </Header.Dropdown>
+              </Header.Profile>
+            </Header.Group>
+          </Header.Frame>
+  
+          <Header.Feature>
+            <Header.FeatureCallOut>Watch Joker Now</Header.FeatureCallOut>
+            <Header.Text>
+              Forever alone in a crowd, failed comedian Arthur Fleck seeks connection as he walks the streets of Gotham
+              City. Arthur wears two masks -- the one he paints for his day job as a clown, and the guise he projects in a
+              futile attempt to feel like he's part of the world around him.
+            </Header.Text>
+            <Header.PlayButton>Play</Header.PlayButton>
+          </Header.Feature>
+        </Header>
+  
+        <Card.Group>
+          {slideRows.map((slideItem) => (
+            <Card key={`${category}-${slideItem.title.toLowerCase()}`}>
+              <Card.Title>{slideItem.title}</Card.Title>
+              <Card.Entities>
+                {slideItem.data.map((item) => (
+                  <Card.Item key={item.docId} item={item}>
+                    <Card.Image src={`/images/${category}/${item.genre}/${item.slug}/small.jpg`} />
+                    <Card.Meta>
+                      <Card.SubTitle>{item.title}</Card.SubTitle>
+                      <Card.Text>{item.description}</Card.Text>
+                    </Card.Meta>
+                  </Card.Item>
+                ))}
+              </Card.Entities>
+              <Card.Feature category={category}>
+              </Card.Feature>
+            </Card>
+          ))}
+        </Card.Group>
+      </>
+    ) : (
+      <SelectProfileContainer user={user} setProfile={setProfile} />
+    );
+  };
